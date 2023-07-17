520cdc864424ddeb27aa45290ab07f3e81fddd4a
Most-Rima-Khatun <113694175+Most-Rima-Khatun@users.noreply.github.com>
2023-07-17 23:21:54 +0600

    Add files via upload

---
+++ GreetingsRunner.java
@@ -0,0 +1,13 @@
+package Greetings.java;
+
+import Greetings.Greetings;
+
+public class GreetingsRunner {
+    public static void main(String[] args) {
+        Greetings greetings = new Greetings();
+        greetings.Hello();
+        greetings.Sagotom();
+        greetings.Nihouw();
+        greetings.Svogaat();
+    }
+}
