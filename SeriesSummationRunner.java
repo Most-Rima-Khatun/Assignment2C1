cec0cccdca127854cd02f47de4342afb0a2b7eff
Most-Rima-Khatun <113694175+Most-Rima-Khatun@users.noreply.github.com>
2023-07-17 23:48:37 +0600

    Add files via upload

---
+++ SeriesSummationRunner.java
@@ -0,0 +1,9 @@
+package SeriesSummation.java;
+
+public class SeriesSummationRunner {
+    public static void main(String[] args) {
+        SeriesSummation seriesSummation = new SeriesSummation();
+        int result = seriesSummation.summation(10);
+        System.out.println(result);
+    }
+}
