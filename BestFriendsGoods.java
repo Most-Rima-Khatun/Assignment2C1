27569fb33f9aaa3051fea1b6f6fd80a194650556
Most-Rima-Khatun <113694175+Most-Rima-Khatun@users.noreply.github.com>
2023-07-18 00:44:36 +0600

    Add files via upload

---
+++ BestFriendsGoods.java
@@ -0,0 +1,33 @@
+package BestFrindsGoods;
+
+public class BestFriendsGoods {
+    private String item,name,movies;
+
+    public void Item1()
+    {
+        item = "Juck_fruits";
+        name = "Faria";
+        movies = "Flight 914";
+        System.out.println(item);
+        System.out.println(name);
+        System.out.println(movies);
+    }
+    public void Item2()
+    {
+        item = "Mango";
+        name = "Aitee";
+        movies = "Alien";
+        System.out.println(item);
+        System.out.println(name);
+        System.out.println(movies);
+    }
+    public void Item3()
+    {
+        item = "Lichi";
+        name = "Jarin";
+        movies = "Luci";
+        System.out.println(item);
+        System.out.println(name);
+        System.out.println(movies);
+    }
+}
---
+++ BestFriendsGoodsRunner.java
@@ -0,0 +1,15 @@
+package BestFrindsGoods;
+
+public class BestFriendsGoodsRunner {
+    public static void main(String[] args) {
+        BestFriendsGoods bestFriendsGoods =new BestFriendsGoods();
+        bestFriendsGoods.Item1();
+        System.out.println();
+        bestFriendsGoods.Item2();
+        System.out.println();
+        bestFriendsGoods.Item3();
+
+    }
+
+
+}
