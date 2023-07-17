dfbeec4e88759260ce804e8c251625e70f1e30ad
Most-Rima-Khatun <113694175+Most-Rima-Khatun@users.noreply.github.com>
2023-07-17 23:20:36 +0600

    Add files via upload

---
+++ Greetings.java
@@ -0,0 +1,18 @@
+package Greetings;
+
+public class Greetings {
+    public void Hello(){
+        System.out.println("Hello");
+    }
+    public void Sagotom(){
+        System.out.println("স্বাগতম");
+    }
+    public void Nihouw(){
+        System.out.println("欢迎");
+    }
+    public void Svogaat()
+    {
+        System.out.println("स्वागत");
+    }
+
+}
